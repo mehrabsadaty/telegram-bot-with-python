3df5540e38807605d06e1cd652410927bdcf5d7f
mehrabsadaty <seiedmehrabsadati@gmail.com>
2025-11-25 00:11:15 +0330

    add button

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,8 @@ def my_handler (client : Client , massage : Message):
                        reply_markup= InlineKeyboardMarkup(
                            [
                                [InlineKeyboardButton(text="button1" , callback_data="button1")] , [InlineKeyboardButton(text="button2" , callback_data="button2")] ,
-                               [InlineKeyboardButton(text="button3" , url="https://google.com")]
+                               [InlineKeyboardButton(text="button3" , url="https://google.com")],
+                               [InlineKeyboardButton(text="click on" , callback_data="click")]
                            ]
                        ) 
                        )
